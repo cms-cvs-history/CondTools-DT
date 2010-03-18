f23502413bed91cf961da9a307e0b2d94da8cdd3
Paolo Ronchese <sha1-591dc25f43b7b0cdf7a5025ce38797ed3141c081@cern.ch>
2010-03-18 16:10:03 +0000

    test applications to read keyed configurations

---
+++ test/stubs/DTKeyedConfigDump.h
@@ -0,0 +1,32 @@
+
+/*----------------------------------------------------------------------
+
+Toy EDAnalyzer for testing purposes only.
+
+----------------------------------------------------------------------*/
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+//#include "CondFormats/DTObjects/interface/DTConfigList.h"
+//#include "CondTools/DT/interface/DTConfigHandler.h"
+//#include "CondTools/DT/interface/DTDBSession.h"
+
+#include <string>
+
+namespace edmtest {
+  class DTKeyedConfigDump : public edm::EDAnalyzer
+  {
+  public:
+    explicit  DTKeyedConfigDump(edm::ParameterSet const& p);
+    explicit  DTKeyedConfigDump(int i) ;
+    virtual ~ DTKeyedConfigDump();
+    virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
+  private:
+    bool dumpCCBKeys;
+    bool dumpAllData;
+  };
+}
